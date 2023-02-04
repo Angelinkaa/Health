ad0f5fe51f404307726f285e04b51f8b9842d462
Angelina <angelinamscw@gmail.com>
2023-02-04 15:29:53 +0300

    Initial commit

---
+++ app/src/main/java/ru/angelinamscw/myapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.angelinamscw.myapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity(args: Array<String>) : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+//    val userPressure = mutableListOf<Int>()
+//    val userPulse = mutableListOf<Int>()
+}
